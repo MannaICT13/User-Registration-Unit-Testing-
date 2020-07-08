54ea7bd5778d540b6b35d19e9d051bf80e61bf00
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-07-08 17:09:30 +0600

    completed all Unit test

--- User Registration(Unit Testing)/UserModel.swift
+++ User Registration(Unit Testing)/UserModel.swift
@@ -10,11 +10,41 @@ import Foundation
 
 struct UserModel{
     
-    let name : String
+    let username : String
     let city : String
+    let email : String
     let password : String
     let repeatPassword: String
     
     
     
+}
+
+extension UserModel{
+
+    func isValidateName() -> Bool{
+        return username.count > 1
+    }
+    
+    func isValidateCity() -> Bool{
+        return city.count > 1 && city.count < 10
+    }
+    
+    func isValidateEmail() -> Bool {
+        return email.contains("@") && email.contains(".")
+    }
+    
+    func isValidPasswordLength() ->Bool{
+        return password.count >= 8 && password.count <= 16
+    }
+    
+    func isMatchPassword() -> Bool{
+        return password == repeatPassword
+    }
+    
+    func isValidPassword() -> Bool{
+        return isValidPasswordLength() && isMatchPassword()
+    }
+    
+
 }
--- User Registration(Unit Testing)Tests/User_Registration_Unit_Testing_Tests.swift
+++ User Registration(Unit Testing)Tests/User_Registration_Unit_Testing_Tests.swift
@@ -10,12 +10,24 @@ import XCTest
 @testable import User_Registration_Unit_Testing_
 
 class User_Registration_Unit_Testing_Tests: XCTestCase {
+    
+    
+    
+    var model : UserModel!
+    
+    let username = "Manna"
+    let city = "Rangpur"
+    let email = "manna@email.com"
+    let password = "12345678"
+    let repeatPassword  = "12345678"
 
     override func setUp() {
        
     }
 
     override func tearDown() {
+        
+        model = nil
      
     }
 
@@ -23,11 +35,45 @@ class User_Registration_Unit_Testing_Tests: XCTestCase {
     
     func test_UserModel(){
         
-        let um = UserModel(name: "Manna", city: "Rangpur", password: "1234", repeatPassword: "1234")
+        model = UserModel(username: username, city: city, email: email, password: password, repeatPassword: repeatPassword)
         
-        XCTAssertNotNil(um)
+        XCTAssertNotNil(model)
+      
+    }
+    
+    func test_isValidateName(){
+        
+        model = UserModel(username: "ss", city: city, email: email, password: password, repeatPassword: repeatPassword)
+        XCTAssertTrue(model.isValidateName())
+        
         
     }
     
+    func test_isValidateCity(){
+        model = UserModel(username: name, city: "Rajshahi", email: email, password: password, repeatPassword: repeatPassword)
+        XCTAssertTrue(model.isValidateCity())
+    }
+    
+    func test_isValidateEmail(){
+        model = UserModel(username: username, city: city, email: "hello@gmail.com", password: password, repeatPassword: repeatPassword)
+        
+        XCTAssertTrue(model.isValidateEmail())
+    }
 
+    func test_isValidPasswordLength(){
+        
+        model = UserModel(username: username, city: city, email: email, password: "12345678", repeatPassword: repeatPassword)
+        
+        XCTAssertTrue(model.isValidPassword())
+    }
+    
+    func test_isMatchPassword(){
+        model = UserModel(username: username, city: city, email: email, password: "1234567", repeatPassword: "1234567")
+        XCTAssertTrue(model.isMatchPassword())
+        
+    }
+    func test_isValidPassword(){
+        model = UserModel(username: username, city: city, email: email, password: "123123123", repeatPassword: "123123123")
+        XCTAssertTrue(model.isValidPassword())
+    }
 }
