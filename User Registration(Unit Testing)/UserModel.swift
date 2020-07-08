40694be30ff5ae6c94eea2dea7c773bd5f080ada
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-07-08 16:21:57 +0600

    Created UserModel and executed first Test

---
+++ User Registration(Unit Testing)/UserModel.swift
@@ -0,0 +1,20 @@
+//
+//  UserModel.swift
+//  User Registration(Unit Testing)
+//
+//  Created by Md Khaled Hasan Manna on 8/7/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import Foundation
+
+struct UserModel{
+    
+    let name : String
+    let city : String
+    let password : String
+    let repeatPassword: String
+    
+    
+    
+}
--- User Registration(Unit Testing)Tests/User_Registration_Unit_Testing_Tests.swift
+++ User Registration(Unit Testing)Tests/User_Registration_Unit_Testing_Tests.swift
@@ -12,23 +12,22 @@ import XCTest
 class User_Registration_Unit_Testing_Tests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+       
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+     
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    
+    func test_UserModel(){
+        
+        let um = UserModel(name: "Manna", city: "Rangpur", password: "1234", repeatPassword: "1234")
+        
+        XCTAssertNotNil(um)
+        
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
